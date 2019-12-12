5ad41af4877c6a57c59c53e0179e468c93087d85
Engström Stefan <engstste@TRAFI.INTRA>
2019-12-12 15:53:43 +0200

    Updaed GML-printer

--- print/S100GmlPrinter.php
+++ print/S100GmlPrinter.php
@@ -6,6 +6,7 @@ class S100GmlPrinter
     private $rootClass;
     private $xml;
     private $ns = "127";
+    private $refCounter = 1;
     
     function __construct ($class)
     {
@@ -54,21 +55,52 @@ class S100GmlPrinter
                 
                 //if Complex attribute, no final values
                if ($instance instanceOf ComplexAttributeType)
-                {
+               {
                     $ns = '';
-                    $baseType = $attribute['type'];
-                    //featuretype or informationtype
-                    if ($baseType == 'FeatureType' || $baseType == 'InformationType')
+                    
+                    //If instance is a FeatureType or InformationType, it shall be included as new <imember> with references;
+                    if ($instance instanceOf FeatureType || $instance instanceOf InformationType)
                     {
-                        //$parentNode = $parentNode->addChild('imember');
+                        //TODO
+                        //USE GML ID:s for references
+                        //Use rolenames
+                        //Always add references as XLINK
+                        //Only print feature once (put printed GML ID in array and check)
+                        
                         $ns = 'S127_';
+                        //add current feature as a new iMember at root- level
+                        $newParentNode = $this->xml->addChild('imember');
+                        $node = $newParentNode->addChild($ns.get_class($instance)); // get the type of the object
+                        
+                        //do not add reference fr top-level objects
+                        if ($parentNode != $this->xml)
+                        {
+                            //The current $attribute holds the reference
+                            //The current $attribute['name'] holds the rolenames of the association
+                            
+                            $roles = explode('_', $attribute['name']);
+                            
+                            $ref = $node->addChild('REF_'.$this->refCounter.'_'.$roles[0]);
+                            
+                            //The current object was referenced in the parent
+                            $ref2 = $parentNode->addChild('REF_'.$this->refCounter.'_'.$roles[1]);
+                            $this->refCounter++;
+                        }
                     }
-                    $node = $parentNode->addChild($ns.get_class($instance)); // get the type of the object
+                    
+                    else
+                    {
+                        //add the content of a ComplexAttribute instead of a reference
+                        $node = $parentNode->addChild($ns.get_class($instance)); // get the type of the object
+                    }
+                    
+                    //ADD COMPLEX ATTRIBUTES
                     $this->printObject($instance->getAllAttributes(), $node); //pass node as parent to children
                 }
                 
+                //SIMPLE
                 else
-                {       
+                {   //Add the value of a SimpleAttribute    
                     $node = $parentNode->addChild(get_class($instance), $instance->oPrint()); //print value in node
                 }
             }
