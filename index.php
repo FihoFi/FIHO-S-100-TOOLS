a17ac40a0ce16f28ccbec34c4b130f8e43dfc324
Engström Stefan <engstste@TRAFI.INTRA>
2022-03-30 16:25:15 +0300

    Updated index.php

--- index.php
+++ index.php
@@ -3,7 +3,4 @@
 <h2>/example</h2>
 <table>
 <tr><td><a href="example/S122">S122</a></td><td></td></tr>
-<tr><td><a href="example/S123">S123</a></td><td></td></tr>
-<tr><td><a href="example/S127">S127</a></td><td></td></tr>
-<tr><td><a href="example/S201">S201</a></td><td></td></tr>
 </table>
